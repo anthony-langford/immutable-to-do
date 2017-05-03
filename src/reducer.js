0996bcdc0ccb5ee992e2a361d3987de2b13e5309
Anthony Langford <anthony.langford@gmail.com>
2017-05-03 12:06:14 -0400

    added add todo reducer function

--- src/reducer.js
+++ src/reducer.js
@@ -5,7 +5,7 @@ const init = List([]);
 export default function(todos=init, action) {
   switch(action.type) {
     case 'ADD_TODO':
-      // ...
+      return todos.push(Map(action.payload));
     case 'TOGGLE_TODO':
       // ...
     default:
